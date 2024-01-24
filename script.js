5197e0bc0fce357326d78ed1b1b741da1c841b93
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2024-01-24 16:48:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// script.js
+const userConfirmation = confirm("Do you want to proceed?");
+
+if (userConfirmation) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
